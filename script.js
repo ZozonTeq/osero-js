65eb1bd141bf01e891815d9b1e8ed822933ee032
zo <77091655+ZozonTeq@users.noreply.github.com>
2022-04-05 19:30:05 +0900

    Bot追加

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function setup(){
-    createCanvas(600,800) //幅600 高さ800でキャンバスを生成
+    let canvas  = createCanvas(600,800) //幅600 高さ800でキャンバスを生成
+    canvas.parent('canvas');
     //noStroke(); //枠の消去
 }
 var stage = [
@@ -12,15 +13,40 @@ var stage = [
     0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,]
 let isDebug = false;
-
+let GameOver = false;
+let vsBot = false;
 // スコア変数
 let player1_score = 0 ;
 let player2_score = 0 ;
+let turnCount =  0 ;//置いた回数合計　64がマックス
 let turn = 1;//ターン
-
+let puttableCount = 0 ;
+let assist = true;
 function draw(){
     background(0x22) // 描画をリセット。
-    player1_score = player2_score = 0;
+    puttableCount = player1_score = player2_score = 0;
+
+    if(vsBot && turn == 2){
+        let bestIndex = 0  ;
+        let bestIndexNum =  0 ; 
+        for(let i = 0 ; i < 64 ; i++){
+            if(canPlace(i%8 , Math.floor(i/8) , turn)){
+                if(bestIndex == 0 ){
+                    bestIndex = i  ;
+                }
+                if(bestIndexNum < tryPut(i% 8 , Math.floor(i/8))){
+                    bestIndex =  i ; 
+                    bestIndexNum = tryPut(i% 8 , Math.floor(i/8));
+                } 
+                break;
+            }
+
+        }
+        replaceCell(bestIndex%8,Math.floor(bestIndex/8));
+        turn = (turn == 1) ? 2 : 1;//次のターンにする
+
+    }//BOT
+
     for (let i =  0 ; i < stage.length ; i++){
         fill(0,0xAA,0);
         rect(20 + 70*(i % 8),20 + 70 * Math.floor(i/8) , 70,70);
@@ -39,21 +65,57 @@ function draw(){
             player2_score++;
         }
         if(canPlace(i%8,Math.floor(i/8),turn)){
-            fill(255,255,0);
-            rect(23 + 70*(i % 8),23 + 70 * Math.floor(i/8) , 64,64,50);
+            fill(turn == 1 ? 0 : 0xFF);
+            if(assist)
+                rect(23+35-8 + 70*(i % 8),23 +35-8+ 70 * Math.floor(i/8) , 8,8,50);
+            puttableCount++;
         }
         if(isDebug){
             textSize(15);fill(0x22);text("X:"+i%8+" Y:"+Math.floor(i/8),23+35 + 70*(i % 8),23 + 35+70 * Math.floor(i/8) )
         }
     }//リバーシ描画とスコア計測
+
+    if(puttableCount == 0 ){
+        for(let i = 0 ; i < 64 ; i++){
+            if(canPlace(i%8,Math.floor(i/8),turn == 1 ? 2 : 1)){
+                puttableCount ++;
+            }
+        }
+        if(puttableCount == 0 ){
+            GameOver = true;
+        }//どっちとも置けないとき。
+        else{
+            turn = (turn == 1) ? 2 : 1;//次のターンにする
+        }
+
+    }
+
     fill(0xFF);textSize(32);textAlign(CENTER, CENTER);
-    text("PLAYER1 : "+player1_score +"          PLAYER2 : "+player2_score,width /2 ,700);
+    text("PLAYER 1",width /4 , 630); text("PLAYER 1",width /4*3 , 630);
+    textSize(64);
+    text(player1_score,width /4 ,   700); 
+    text(player2_score,width /4*3 , 700);
+    textSize(32);
     text("turn:"+turn+(turn == 1 ? "black" : "white "),width/2 , 730)
+    if(GameOver || turnCount >= 64){
+        background(0x22);
+        textSize(63);
+        text("*GAMEOVER*" , width / 2 , height /2-300);
+
+        textSize(32);
+        text(player1_score > player2_score ? "BLACK WIN!" : player2_score == player1_score ? "DRAW" : "WHITEWIN!", width / 2 , height /2-100);
+        text("PLAYER 1",width /4 , 630); text("PLAYER 1",width /4*3 , 630);
+        textSize(64);
+        text(player1_score,width /4 ,   700); 
+        text(player2_score,width /4*3 , 700);
+        textSize(32);
+    }
 
     //スコア描画
 }
 
-function mouseClicked(){
+
+function touchStarted() {
     let x = 0 ,  y = 0 ;
     x = Math.floor(mouseX);
     y = Math.floor(mouseY);
@@ -68,8 +130,12 @@ function mouseClicked(){
     if(getCell(arrX,arrY) === undefined){
         return ;
     }
-    replaceCell(arrX,arrY);
-    turn = (turn == 1) ? 2 : 1;//次のプレイヤーにターンを渡す。
+    if(canPlace(arrX,arrY,turn)){
+        replaceCell(arrX,arrY);
+        turn = (turn == 1) ? 2 : 1;//次のプレイヤーにターンを渡す。
+    }
+    //ここに入力を受け取る処理を書く
+
 }
 function getCell(x,y){
     return stage[y*8 + x];
@@ -204,6 +270,48 @@ function replaceCell(x,y){
         }
     }
 }
+
+function tryPut(x,y){
+    if(x > 8 || y > 8 || x < 0 || y <0){
+        return;
+    }
+    if(canPlace(x,y,turn)){
+
+        let rres = 0  ;
+        for_i :for(let i = 1  ; i <= 8 ; i ++){
+            let res = false;
+            let pnum = 0;
+            let nres = false;//おけることが確定か？じゃないか？
+            arr = getCells(x,y,i);
+            if(arr.length > 3){
+                if(arr[1]==(turn == 1 ? 2 : 1)){//二番目が敵の石か？
+                    for_j : for(let j = 1 ; j < arr.length; j++){
+                        if(!nres){
+                            if(arr[j] == 0 ){
+                                continue for_j;
+                            }//２番目以降に何もないならおけない
+                            if (arr[j] == (turn == 1 ? 2 : 1)){
+                                res = true;
+                            }//てきのやつか？
+                            else if(arr[j] == turn){
+                                res = true;
+                                pnum = j;
+                                nres = true;
+                                console.log("checking"  +i);
+                            }//自分のか
+                        }
+
+                    }
+                }
+            }
+            if(nres){
+                rres += pnum;
+                continue for_i;
+            }  
+        }
+        return rres;
+    }
+}//そのマスがどのくら いとれるの確認
 
 
 function canPlace(x,y,t){
